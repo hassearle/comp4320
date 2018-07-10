682ab90ee76265ac7f1d06b11fdf812611b1bd00
Unknown <a.searle274@gmail.com>
2018-07-10 11:59:01 -0500

    Lim's skeleton code
    slides Ch2. 55-58

---
+++ UDPClient.java
@@ -0,0 +1,42 @@
+import java.io.*; 
+import java.net.*; 
+  
+class UDPClient { 
+    public static void main(String args[]) throws Exception 
+    { 
+      //create input stream
+      BufferedReader inFromUser = 
+        new BufferedReader(new InputStreamReader(System.in)); 
+  
+      //create client socket
+      DatagramSocket clientSocket = new DatagramSocket(); 
+  
+      //translate hostname to IP address USING DNS 
+      InetAddress IPAddress = InetAddress.getByName("hostname"); 
+  
+      byte[] sendData = new byte[1024]; 
+      byte[] receiveData = new byte[1024]; 
+  
+      String sentence = inFromUser.readLine(); 
+      sendData = sentence.getBytes();         
+
+      //create datagram with data-to-send, length, IP addr, port
+      DatagramPacket sendPacket = 
+         new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
+  
+      //send datagram to server
+      clientSocket.send(sendPacket); 
+  
+      DatagramPacket receivePacket = 
+         new DatagramPacket(receiveData, receiveData.length); 
+  
+      //read datafram from server
+      clientSocket.receive(receivePacket); 
+  
+      String modifiedSentence = 
+          new String(receivePacket.getData()); 
+  
+      System.out.println("FROM SERVER:" + modifiedSentence); 
+      clientSocket.close(); 
+      } 
+} 
---
+++ UDPServer.java
@@ -0,0 +1,42 @@
+import java.io.*; 
+import java.net.*; 
+  
+class UDPServer { 
+  public static void main(String args[]) throws Exception 
+    { 
+      //create datagram socket at port 9876
+      DatagramSocket serverSocket = new DatagramSocket(9876); 
+  
+      byte[] receiveData = new byte[1024]; 
+      byte[] sendData  = new byte[1024]; 
+  
+      while(true) 
+        { 
+        //create space for recieved datagram
+          DatagramPacket receivePacket = 
+             new DatagramPacket(receiveData, receiveData.length); 
+           serverSocket.receive(receivePacket); 
+
+        //recieve datagram
+            String sentence = new String(receivePacket.getData()); 
+  
+        //Get IP addr port #, of sender
+          InetAddress IPAddress = receivePacket.getAddress(); 
+        //^
+          int port = receivePacket.getPort(); 
+  
+                      String capitalizedSentence = sentence.toUpperCase(); 
+
+          sendData = capitalizedSentence.getBytes(); 
+  
+          //create datagram to send to client
+          DatagramPacket sendPacket = 
+             new DatagramPacket(sendData, sendData.length, IPAddress, 
+                               port); 
+
+         //write out datagram to socket
+          serverSocket.send(sendPacket); 
+        }
+        //end of while loop, loop pack and wait for another datagram 
+    } 
+}  
