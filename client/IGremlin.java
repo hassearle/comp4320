346aa65c4e77e3a402d8a37f508d2096440004da
Ash <a.searle274@gmail.com>
2018-07-13 18:20:22 -0500

    GremlinImple.ttlCorrupt()
    prob of 1/2/3 bytes being damaged

--- client/IGremlin.java
+++ client/IGremlin.java
@@ -1,6 +1,9 @@
 package client;
 import java.net.DatagramPacket;
+
 public interface IGremlin {
 	// corrupts a packet if the provided probability is met
-	DatagramPacket corruptPackets(DatagramPacket packet, float probability);
+   DatagramPacket corruptPackets(DatagramPacket packet, float probability);
+   int pcktSz = 256;
+   //int corruptsLeft = -1;
 }
