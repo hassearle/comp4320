e330f993941dbd2a66f2d0da5818f6d720b7c3fd
Justin Sawyer <jtsaw36@gmail.com>
2018-07-15 19:30:41 -0500

    added functionality to write to file

--- client/UDPClient.java
+++ client/UDPClient.java
@@ -28,12 +28,15 @@ public class UDPClient {
       IErrorDetection errorDetec = new ErrorDetectionImpl();
       ISegmentation segmentation = new SegmentationImpl();
       float probabilityOfError = 0.3f;
+      ArrayList<byte> dataWriteToFile = new ArrayList<byte>;
+      byte[] dataWriteToFileArr = new byte[1];
       int highestSequenceReceived;
       clientSocket.send(sendPacket);
       System.out.println("Client sent HTTP request");
       DatagramPacket receivePacket, corruptedPacket; 
       ArrayList<DatagramPacket> packets = new ArrayList<DatagramPacket>();
-      DatagramPacket[] packetsArr = new DatagramPacket[1];
+      DatagramPacket[] packetsArr = new DatagramPacket[1],
+        reassemblePacketsArr = new DatagramPacket[1];
       boolean isCorrupt = false, nullArrived = false, allArrived = false;
       while(!allArrived && !nullArrived) {
         receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -65,8 +68,15 @@ public class UDPClient {
             allArrived = true;
           }
         }
-        segmentation.reassemblePackets(packetsArr, packetsArr.length);
-
+      }
+      reassemblePacketsArr = segmentation.reassemblePackets(packetsArr, packetsArr.length);
+      for(int i = 0; i < packetsArr.length; i++){
+        for(int j = 0; j < packetsArr[i].length; j++)
+          dataWriteToFile.add(packetsArr[i].getData[j]);
+      }
+      dataWriteToFileArr = new byte[dataWriteToFileArr.size()];
+      for(int i = 0; i < dataWriteToFile.size(); i++){
+        dataWriteToFileArr[i] = dataWriteToFile.get(i);
       }
     }
 
@@ -78,5 +88,16 @@ public class UDPClient {
             sum += (int) b;
         }
         return sum;
+    }
+
+    public boolean writeDataToFile(byte[] data, String fileName) {
+        try {
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(data);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
